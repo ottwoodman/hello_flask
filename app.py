a0600f9ad15031cfbc323448988640e000e4797f
ott <ottwoodman@gmail.com>
2019-11-30 22:39:53 +0800

    bug fixed: missing import os

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 
@@ -17,3 +19,5 @@ def hello():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
